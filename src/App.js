f3004f86bd1d21450180d9df870ab19c79c6d734
Nhamakami7 <nicholas_hamakami7@outlook.com>
2021-04-26 15:00:44 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function snack() {
+  return "Cereal";
+}
+
+function App() {
+  const greeting = "Hello";
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>{greeting}</h1>
+        <h1>My midnight snack is {snack()}</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React. Sup cuddi 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
